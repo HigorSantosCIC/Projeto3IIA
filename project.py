d92155510164dbed831153d284c774081675d8a7
leonardorodriguesds <leoonardodf@gmail.com>
2020-11-05 01:06:19 -0300

    update

--- project.py
+++ project.py
@@ -13,34 +13,34 @@ from sklearn.tree import export_graphviz,plot_tree
 data = pd.read_excel('data/dataset.xlsx')
 
 predictors = data.iloc[:, 1:74]
-predictors = predictors.drop(columns=['Patient age quantile', 'SARS-Cov-2 exam result', 'Mycoplasma pneumoniae', 'Urine - pH'])
+predictors = predictors.drop(columns=['Patient addmited to regular ward (1=yes, 0=no)', 'Patient addmited to semi-intensive unit (1=yes, 0=no)', 'Patient addmited to intensive care unit (1=yes, 0=no)', 'Patient age quantile', 'SARS-Cov-2 exam result', 'Mycoplasma pneumoniae', 'Urine - pH'])
 columns_predictors = predictors.columns
-classes = data.iloc[:,2].values
+classes = data.iloc[:,5].values
 
 predictors.info()
 predictors = predictors.values
 
 simpleImputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
-predictors[:,0:18] = simpleImputer.fit_transform(predictors[:,0:18])
-predictors[:,35:41] = simpleImputer.fit_transform(predictors[:,35:41])
-predictors[:,43:51] = simpleImputer.fit_transform(predictors[:,43:51])
-predictors[:,52:67] = simpleImputer.fit_transform(predictors[:,52:67])
+predictors[:,0:15] = simpleImputer.fit_transform(predictors[:,0:15])
+predictors[:,32:38] = simpleImputer.fit_transform(predictors[:,32:38])
+predictors[:,40:48] = simpleImputer.fit_transform(predictors[:,40:48])
+predictors[:,49:64] = simpleImputer.fit_transform(predictors[:,49:64])
 
 categoricalImputer = CategoricalImputer()
-predictors[:,18:24] = categoricalImputer.fit_transform(predictors[:,18:24])
-predictors[:,24:35] = categoricalImputer.fit_transform(predictors[:,24:35])
-predictors[:,41:43] = categoricalImputer.fit_transform(predictors[:,41:43])
-predictors[:,67:69] = categoricalImputer.fit_transform(predictors[:,67:69])
-predictors[:,51] = categoricalImputer.fit_transform(predictors[:,51])
+predictors[:,15:21] = categoricalImputer.fit_transform(predictors[:,15:21])
+predictors[:,21:32] = categoricalImputer.fit_transform(predictors[:,21:32])
+predictors[:,38:40] = categoricalImputer.fit_transform(predictors[:,38:40])
+predictors[:,64:66] = categoricalImputer.fit_transform(predictors[:,64:66])
+predictors[:,48] = categoricalImputer.fit_transform(predictors[:,48])
 
 labelEncoder = LabelEncoder()
-for x in [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 41, 42, 67, 68, 51]:
+for x in [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 38, 39, 64, 65, 48]:
     predictors[:,x] = labelEncoder.fit_transform(predictors[:,x])
 print(predictors)
 # pandas_profiling.ProfileReport(classes)
 train_x,test_x,train_y,test_y = train_test_split(predictors, classes)
 
-clf = RandomForestClassifier(max_depth=10, random_state=0)
+clf = RandomForestClassifier(max_depth=3, random_state=0)
 clf.fit(train_x, train_y)
 
 results = clf.predict(test_x)
@@ -66,11 +66,11 @@ for i,v in sorted(enumerate(importance), key=lambda i: i[1], reverse=True)[:10]:
 
 columns_predictors = list(map(
     lambda i: columns_predictors[i[0]],
-    sorted(enumerate(importance), key=lambda i: i[1], reverse=False)[:10]
+    sorted(enumerate(importance), key=lambda i: i[1], reverse=True)[:10]
 ))
 plot_importances = list(map(
     lambda x: x[1],
-    sorted(enumerate(importance), key=lambda i: i[1], reverse=False)[:10]
+    sorted(enumerate(importance), key=lambda i: i[1], reverse=True)[:10]
 ))
 plt.rc('font', size=8)
 plt.bar(columns_predictors, plot_importances)
